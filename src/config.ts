38a8e43452d54f1059c6b20d778ac658412ad99d
hojinyoon <hojinyoon101@gmail.com>
2025-12-27 19:34:59 -0500

    Update

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Hojin Yoon - Portfolio';
+export const SITE_DESCRIPTION = 'Computational Data Science at Penn State';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
